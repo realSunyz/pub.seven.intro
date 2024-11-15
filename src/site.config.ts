66cbe982086d626fc347d63847c11dbd9fe1de9e
Seven <i@sunyz.net>
2024-11-15 21:53:24 +0800

    [±] npm lint

--- src/site.config.ts
+++ src/site.config.ts
@@ -5,7 +5,7 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: 'realSunyz',
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: 'it\'s 7 here',
+	title: "it's 7 here",
 	// Meta property used as the default description meta property
 	description: 'Everything about Yanzheng SUN (aka. Seven SUN, realSunyz).',
 	// HTML lang property, found in src/layouts/Base.astro L:18
